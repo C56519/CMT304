9e699b766b8432c92efc46e1957d53cff7aa1a48
C56519 <chenw56519@gmail.com>
2024-03-25 22:24:22 +0000

    finished: coursework02

--- 02Functional Language/coursework01.hs
+++ 02Functional Language/coursework01.hs
@@ -5,20 +5,24 @@ type BinaryImage = [[Int]]
 type Coord = (Int, Int)
 -- 1.3 A visit record list, recording whether each pixel has been visited.
 type Visited = [[Bool]]
--- 工具函数
--- 判断是否在边界内
-ifInBounds :: Coord -> Bool
-ifInBounds (x, y) = x >= 0 && y >= 0 && x < rowsNum && y < colsNum
+
 -- 深度优先搜索
 -- 使用坐标而非整个图进行DFS
 dfs :: BinaryImage -> Coord -> Int -> [[Bool]] -> (Int, [[Bool]])
-dfs image (x, y) v visited
-  | not (ifInBounds (x, y)) || visited !! x !! y || image !! x !! y /= v = (0, visited)
-  | otherwise = let
-      newVisited = (take x visited) ++ [take y (visited !! x) ++ [True] ++ drop (y + 1) (visited !! x)] ++ (drop (x + 1) visited)
-      directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-      (count, finalVisited) = foldl (\(c, vst) (dx, dy) -> let (nc, nvst) = dfs image (x + dx, y + dy) v vst in (c + nc, nvst)) (1, newVisited) directions
-    in (count, finalVisited)
+dfs image (x, y) v visited rows cols
+    | not (ifInBounds (x, y)) || visited !! x !! y || image !! x !! y /= v = (0, visited)
+    | otherwise = let
+        newVisited = (take x visited) ++ [take y (visited !! x) ++ [True] ++ drop (y + 1) (visited !! x)] ++ (drop (x + 1) visited)
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        (count, finalVisited) = foldl (\(c, vst) (dx, dy) -> let (nc, nvst) = dfs image (x + dx, y + dy) v vst in (c + nc, nvst)) (1, newVisited) directions
+      in (count, finalVisited)
+    where
+        ifInBounds :: Coord -> Bool
+        ifInBounds (x, y) = x >= 0 && y >= 0 && x < rows && y < cols
+        updateVisited :: Int -> Int -> Visited -> Visited
+        updateVisited x y visited = 
+          take x visited ++ [take y (visited !! x) ++ [True] ++ drop (y + 1) (visited !! x)] ++ drop (x + 1) visited
+
 
 -- 主函数，开始DFS并找到最大连通组件
 nlcc :: BinaryImage -> Int -> Int
